34b802ac661ae6c1155f9194f3dacad8f7418c2a
Alan Coiffard <alan.coiffar@gmail.com>
2025-03-17 13:44:16 +0100

    bing solution

--- bing_search.py
+++ bing_search.py
@@ -1,34 +1,40 @@
 from bing_image_downloader import downloader
 import json
 
+import sys
+json_dir = ""
+if(len(sys.argv) < 2):
+    print("Error: please add a json file to use")
+    exit(-1)
+json_dir = sys.argv[1]
+
+print (json_dir)
 
 def download(query="", limit=100, output_dir=""):
     if query != "":
         downloader.download(
             query, 
             limit,  
-            output_dir = "dataset/" + output_dir, 
+            output_dir = "dataset/" + json_dir + "/" + output_dir, 
             adult_filter_off=True, 
             force_replace=True, 
             timeout=60)
+        
 
-with open('./json/tags.json', encoding='utf-8') as f:
+with open('./json/'+ json_dir +'.json', encoding='utf-8') as f:
     json_file = json.load(f)
     
     # Récupérer les sous-catégories pour chaque clé
     for category, elements in json_file.items():
         # print(f"Catégorie: {category}")
-        for subcategory, tags in elements.items():
-            # print(f"  - {subcategory}: {tags}")
-            # print(tags)
-            for tag in tags:
-                print("process : ", tag)
-                output_dir = category + "/" + subcategory
-                # print(output_dir)
-                download(
-                    query= "photography " + tag, 
-                    limit=100, 
-                    output_dir=output_dir)
+        for tag in elements:
+            print("process : ", tag)
+            output_dir = category
+            # print(output_dir)
+            download(
+                query= "photography " + tag, 
+                limit=100, 
+                output_dir=output_dir)
 
 
     # # Boucle pour parcourir chaque clé et ses éléments
